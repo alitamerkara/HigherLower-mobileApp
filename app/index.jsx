0618426c1be3e0720cea6dd8464019aabf30f428
alitamerkara <alitamerkara1907@gmail.com>
2024-08-02 16:38:37 +0300

    bg image added

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,19 @@
-import { StyleSheet, Text, View, StatusBar, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import StartGame from "./screens/StartGame";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="light" translucent />
       <LinearGradient colors={["#bfbfff", "#002d68"]} style={styles.container}>
-        <StartGame />
+        <ImageBackground
+          source={require("../assets/bg.png")}
+          resizeMode="cover"
+          style={styles.container}
+          imageStyle={styles.image}
+        >
+          <StartGame />
+        </ImageBackground>
       </LinearGradient>
     </View>
   );
@@ -15,7 +21,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    marginVertical: -32,
+    marginVertical: -31,
     flex: 1,
+  },
+  image: {
+    opacity: 0.3,
   },
 });
